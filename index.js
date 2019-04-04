e0bb0e6e94a219f782cf6c07d77e7fa3abeef9e9
anderson.bellini <anderson.bellini@anac.gov.br>
2019-04-03 23:31:57 -0300

    initial application

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+  response.render("home");
+});
+
+app.listen(3000, err => {
+  if (err) {
+    console.log("Não encontrado o servidor");
+  } else {
+    console.log("Servidor rodando ...");
+  }
+});
